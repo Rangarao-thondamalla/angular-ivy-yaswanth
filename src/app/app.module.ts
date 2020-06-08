1798c3e8164432984fc601702376a57adc454a27
Rangarao-thondamalla <t.rangarao007@gmail.com>
2020-06-08 18:03:03 +0530

    1 file modified, 3 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,10 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-import { MDBBootstrapModule } from 'angular-bootstrap-md';
-import { MultiItemCarouselComponent } from './multi-item-carousel/multi-item-carousel.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule , MDBBootstrapModule.forRoot()],
-  declarations: [ AppComponent, HelloComponent, MultiItemCarouselComponent ],
+  imports:      [ BrowserModule, FormsModule ],
+  declarations: [ AppComponent, HelloComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
