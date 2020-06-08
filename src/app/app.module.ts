00feb420a50525a43748eb85666fcd01fb3a69cb
Rangarao-thondamalla <t.rangarao007@gmail.com>
2020-06-08 17:45:14 +0530

    3 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { MDBBootstrapModulesPro, MDBSpinningPreloader } from 'ng-uikit-pro-standard';
+import { MultiItemCarouselComponent } from './multi-item-carousel/multi-item-carousel.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, MultiItemCarouselComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
